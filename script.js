5f82e09d67e5b5605c43f8eae82802f8a0be19f3
liammahoney115 <liammahoney115@gmail.com>
2022-09-03 05:25:01 +0000

    Creating script and naming variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let myScore = 0;
+let score_element = document.getElementById("score")
+let question_element= document.getElementById("question")
+let answerA =document.getElementById("answer-a");
+let answerB =document.getElementById("answer-b");
+let answerC =document.getElementById("answer-c");
+let answerD =document.getElementById("answer-d");
+let question = 1;
